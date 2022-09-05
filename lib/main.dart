c4035ffea6bd86364fde9e1a9fe2417629522c87
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-09-05 09:58:59 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: LandingScreen(),
+      home: const LandingScreen(),
       routes: {
         '/signin': (context) => const SignInScreen(),
         '/signup': (context) => const SignUpScreen(),
